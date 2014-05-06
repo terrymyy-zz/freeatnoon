67f742921f7376f0ad7a80f3ae6875739ca6be7e
Hong Kim <honki91@gmail.com>
2014-05-06 15:30:56 -0400

    js view switch

--- app/assets/javascripts/landing_pages.js
+++ app/assets/javascripts/landing_pages.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
     $("#user_phone_number").mask("(999) 999-9999");
+
+    $("#join-group-button").click(function(e) {
+	e.preventDefault();
+        $("#user-info-form").show();
+        $("#welcome-message").hide();
+    });
 });
