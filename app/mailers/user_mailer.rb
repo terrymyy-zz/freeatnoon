8770ba43d7eb21ffca61a415a5e1fb83836d719c
Hong Kim <honki91@gmail.com>
2014-09-23 02:09:09 -0400

    change mailer to just ActionMailer::Base, also add from field

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,4 +1,5 @@
-class UserMailer < Devise::Mailer
+class UserMailer < ActionMailer::Base
+  default from: "AmyGutmann@freeatnoon.com"
   def send_time_input_reminder(user)
     @user = user
     mail(to: @user.email, subject: "F@N - Tell us when you are free!")
