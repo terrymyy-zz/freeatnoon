0ed87009858c3e32a3e1096762f858783a9ed834
Hong Kim <honki91@gmail.com>
2014-09-24 00:12:14 -0400

    revert devise mailer back to itself

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -13,7 +13,7 @@ Devise.setup do |config|
   config.mailer_sender = 'AmyGutmann@freeatnoon.com'
 
   # Configure the class responsible to send e-mails.
-  config.mailer = 'UserMailer'
+  # config.mailer = 'UserMailer'
 
   # ==> ORM configuration
   # Load and configure the ORM. Supports :active_record (default) and
